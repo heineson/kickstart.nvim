aae1054b7eca5b310a1dbfb097357f1f3b53aae2
Jonas Heineson <heineson@gmail.com>
2024-11-01 14:30:58 +0100

    Added barbar bufferline/tabline plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,24 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'romgrk/barbar.nvim',
+    dependencies = {
+      'lewis6991/gitsigns.nvim',
+      'nvim-tree/nvim-web-devicons',
+    },
+    init = function()
+      vim.g.barbar_auto_setup = false
+    end,
+    opts = {
+      -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+      -- animation = true,
+      -- insert_at_start = true,
+      -- …etc.
+      auto_hide = 1,
+      separator = { left = '▎', right = '' },
+    },
+    version = '^1.0.0', -- optional: only update when a new 1.x version is released
+  },
+}
